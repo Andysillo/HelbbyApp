a818a3240b089514487049b802485c27fb664234
Andres Gonzalez <Andres9321Gonzalez@gmail.com>
2017-09-19 17:32:42 -0500

    Se modificaron el inicio de sesion y se agrego activity de recuperar contraseña

--- app/src/main/java/com/helbby/helbbyapp/cabg/LoginEmailActivity.java
+++ app/src/main/java/com/helbby/helbbyapp/cabg/LoginEmailActivity.java
@@ -1,14 +1,32 @@
 package com.helbby.helbbyapp.cabg;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginEmailActivity extends AppCompatActivity {
+
+    Button olvidarContra;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_email);
+
+
+        olvidarContra = (Button) findViewById(R.id.olvidarContra);
+        olvidarContra.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentRegistrarse = new Intent(LoginEmailActivity.this, OlviContActivity.class);
+                startActivity(intentRegistrarse);
+                overridePendingTransition(R.anim.trans_left_in, R.anim.trans_left_out);
+            }
+        });
+
+
     }
 
     @Override
