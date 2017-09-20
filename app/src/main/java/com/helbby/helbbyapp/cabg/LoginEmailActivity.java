7650f8e90ddb8f1ee6286a871e74e748fe48fdbb
Andres Gonzalez <Andres9321Gonzalez@gmail.com>
2017-09-20 09:53:47 -0500

    ya se puede iniciar sesion, hace falta agregarle progrees bar para que se vea la secuencua de cargando

--- app/src/main/java/com/helbby/helbbyapp/cabg/LoginEmailActivity.java
+++ app/src/main/java/com/helbby/helbbyapp/cabg/LoginEmailActivity.java
@@ -1,20 +1,59 @@
 package com.helbby.helbbyapp.cabg;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginEmailActivity extends AppCompatActivity {
 
-    Button olvidarContra;
+    Button olvidarContra, buttonIngresa;
+    EditText etEmail, etPass;
+    FirebaseAuth auth;
+    FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_email);
 
+        auth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(FirebaseAuth firebaseAuth) {
+                if (firebaseAuth.getCurrentUser() != null) {
+                    Intent intent = new Intent(LoginEmailActivity.this, HelbbyActivity.class);
+                    startActivity(intent);
+                    overridePendingTransition(R.anim.trans_zoom_forward_in, R.anim.trans_zoom_forward_out);
+                    finish();
+                    //mAuth.signOut();
+                }
+            }
+        };
+
+        etEmail = (EditText) findViewById(R.id.emailLogin);
+        etPass = (EditText) findViewById(R.id.passwordLogin);
+
+
+        buttonIngresa = (Button) findViewById(R.id.buttonIngresar);
+        buttonIngresa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startRegister();
+            }
+        });
 
         olvidarContra = (Button) findViewById(R.id.olvidarContra);
         olvidarContra.setOnClickListener(new View.OnClickListener() {
@@ -26,6 +65,30 @@ public class LoginEmailActivity extends AppCompatActivity {
             }
         });
 
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        auth.addAuthStateListener(mAuthListener);
+    }
+
+    private void startRegister(){
+        final String email = etEmail.getText().toString();
+        final String password = etPass.getText().toString();
+        if (!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)) {
+            auth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                auth.signInWithEmailAndPassword(email, password);
+                            }
+                        }
+                    });
+        }
 
     }
 
