d4abe034c2fbe7d5e2b0643fe44325d975422975
Andres Gonzalez <Andres9321Gonzalez@gmail.com>
2017-09-20 15:02:21 -0500

    se agregaron los iconos al tab del viewpager.

--- app/src/main/java/com/helbby/helbbyapp/cabg/HelbbyActivity.java
+++ app/src/main/java/com/helbby/helbbyapp/cabg/HelbbyActivity.java
@@ -11,6 +11,13 @@ public class HelbbyActivity extends AppCompatActivity {
 
     TabLayout tabLayoutHelbby;
     ViewPager viewPagerHelbby;
+    private int[] tabIcons = {
+            R.mipmap.ic_launcher,
+            R.mipmap.ic_launcher_round,
+            R.mipmap.ic_launcher,
+            R.mipmap.ic_launcher_round,
+            R.mipmap.ic_launcher
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +31,15 @@ public class HelbbyActivity extends AppCompatActivity {
 
         tabLayoutHelbby.setupWithViewPager(viewPagerHelbby);
         viewPagerHelbby.setAdapter(new AdapterHelbby(getSupportFragmentManager()));
+        setupTabIcons();
+
+    }
+
+    private void setupTabIcons() {
+        tabLayoutHelbby.getTabAt(0).setIcon(tabIcons[0]);
+        tabLayoutHelbby.getTabAt(1).setIcon(tabIcons[1]);
+        tabLayoutHelbby.getTabAt(2).setIcon(tabIcons[2]);
+        tabLayoutHelbby.getTabAt(3).setIcon(tabIcons[3]);
+        tabLayoutHelbby.getTabAt(4).setIcon(tabIcons[4]);
     }
 }
